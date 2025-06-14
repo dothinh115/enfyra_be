2ce17aa791aed51267c29aa50fa333fb3af31ada
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2025-06-15 00:55:05 +0700

    bug fix

--- src/interceptors/hidden-field.interceptor.ts
+++ src/interceptors/hidden-field.interceptor.ts
@@ -5,35 +5,58 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
+import { DataSource, EntityMetadata } from 'typeorm';
 import { HIDDEN_FIELD_KEY } from '../utils/constant';
 
 @Injectable()
 export class HideFieldInterceptor implements NestInterceptor {
+  constructor(private readonly dataSource: DataSource) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(map((data) => this.sanitizeResponse(data)));
+    return next.handle().pipe(map((data) => this.sanitizeDeep(data)));
   }
 
-  sanitizeResponse(data: any): any {
-    if (Array.isArray(data)) {
-      return data.map((item) => this.sanitizeObject(item));
-    } else if (typeof data === 'object' && data !== null) {
-      return this.sanitizeObject(data);
+  private sanitizeDeep(value: any): any {
+    if (Array.isArray(value)) {
+      return value.map((v) => this.sanitizeDeep(v));
     }
-    return data;
+
+    if (value && typeof value === 'object') {
+      const sanitized = this.sanitizeObject(value);
+      for (const key of Object.keys(sanitized)) {
+        sanitized[key] = this.sanitizeDeep(sanitized[key]);
+      }
+      return sanitized;
+    }
+
+    return value;
   }
 
-  sanitizeObject(obj: any): any {
-    const sanitized = { ...obj };
-    const prototype = Object.getPrototypeOf(obj);
-    if (!prototype) return sanitized;
+  private sanitizeObject(obj: any): any {
+    if (!obj || typeof obj !== 'object') return obj;
 
-    for (const key of Object.keys(sanitized)) {
-      const isHidden = Reflect.getMetadata(HIDDEN_FIELD_KEY, prototype, key);
-      if (isHidden) {
-        delete sanitized[key];
+    const sanitized = { ...obj };
+    const matchedMetas = this.findMatchingEntityMetas(obj);
+
+    for (const meta of matchedMetas) {
+      if (typeof meta.target !== 'function') continue;
+      const prototype = meta.target.prototype;
+
+      for (const column of meta.columns) {
+        const key = column.propertyName;
+        const isHidden = Reflect.getMetadata(HIDDEN_FIELD_KEY, prototype, key);
+        if (isHidden) {
+          delete sanitized[key];
+        }
       }
     }
 
     return sanitized;
+  }
+
+  private findMatchingEntityMetas(obj: any): EntityMetadata[] {
+    return this.dataSource.entityMetadatas.filter((meta) =>
+      meta.columns.every((col) => col.propertyName in obj),
+    );
   }
 }
