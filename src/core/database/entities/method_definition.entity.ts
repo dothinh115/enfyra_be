6be4f3abddb85e0d02d30fb20faef27fb01eaa9f
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2025-08-28 07:15:29 +0700

    refactor: remove setting relation from method_definition and update schema
    - Eliminated the 'setting' relation from the 'Method_definition' entity to streamline the schema and improve clarity.
    - Updated the 'snapshot.json' to reflect the removal of the 'setting' relation, enhancing data integrity and simplifying relationship management.

--- src/core/database/entities/method_definition.entity.ts
+++ src/core/database/entities/method_definition.entity.ts
@@ -1,12 +1,10 @@
-import { Entity, Index, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable, ManyToOne, JoinColumn, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { Route_permission_definition } from './route_permission_definition.entity';
 import { Route_definition } from './route_definition.entity';
-import { Setting_definition } from './setting_definition.entity';
 import { Route_handler_definition } from './route_handler_definition.entity';
 import { Hook_definition } from './hook_definition.entity';
 
 @Entity('method_definition')
-@Index(['setting'])
 export class Method_definition {
     @PrimaryGeneratedColumn('increment')
     id: number;
@@ -20,9 +18,6 @@ export class Method_definition {
     @ManyToMany('Route_definition', (rel: any) => rel.publishedMethods, { nullable: true, cascade: true, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
     @JoinTable()
     routes: any;
-    @ManyToOne('Setting_definition', (rel: any) => rel.methods, { nullable: true, onDelete: 'SET NULL', onUpdate: 'CASCADE' })
-    @JoinColumn()
-    setting: any;
     @OneToMany('Route_handler_definition', (rel: any) => rel.method, { cascade: true })
     handlers: any;
     @ManyToMany('Hook_definition', (rel: any) => rel.methods, { nullable: true, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
